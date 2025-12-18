4bfeeedc01ed52c52f08fe1cc9ecfae06911be16
gauravdevkar27 <155799241+gauravdevkar27@users.noreply.github.com>
2025-12-19 00:42:33 +0530

    creating user model , user controller with middleware and cloudinary basic setup

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 import express from "express";
-
 import "dotenv/config";
 import cors from "cors";
 import http from "http";
+import { connectDB } from "./lib/db.js";
 
 //Create express app and http server
 const app = express();
@@ -13,6 +13,8 @@ app.use(express.json({limit: "4mb"}));
 app.use(cors());
 
 app.use("/api/status", (req, res) => res.send("server is live"));
+
+await connectDB();
 
 const PORT = process.env.PORT || 5000
 server.listen(PORT, ()=> console.log("server is running on PORT: "+PORT));
