2db182054398c7bd13035a4255d6e848511945f1
gauravdevkar27 <155799241+gauravdevkar27@users.noreply.github.com>
2025-12-18 18:20:31 +0530

    the port number

--- server/server.js
+++ server/server.js
@@ -14,5 +14,5 @@ app.use(cors());
 
 app.use("/api/status", (req, res) => res.send("server is live"));
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000
 server.listen(PORT, ()=> console.log("server is running on PORT: "+PORT));
