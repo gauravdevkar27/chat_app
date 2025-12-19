e2a0b6f88dfd532969745382814a34eaf657172b
gauravdevkar27 <155799241+gauravdevkar27@users.noreply.github.com>
2025-12-19 19:25:20 +0530

    feat(messages): add schema, controller, and routes

---
+++ server/controllers/message.controller.js
@@ -0,0 +1,77 @@
+import express from "express";
+import User from "../models/User.model.js";
+import Message from "../models/message.model.js";
+//get all users expect the logged in user
+export const getUsersForSidebar = async (req, res) => {
+    try {
+        const userId = req.user._id;
+        const filteredUsers = await User.find({ _id: { $ne: userId } }).select("-password")
+
+        //count no of msgs not seen
+        const unseenMessages = {}
+        const promises = filteredUsers.map(async (user) => {
+            const messages = await Message.find({ senderId: user._id, receiverId: userId, seen: false })
+            if (messages.length > 0) {
+                unseenMessages[user._id] = messages.length;
+            }
+        })
+        await Promise.all(promises);
+        res.json({
+            success: true, users: filteredUsers, unseenMessages
+        })
+    }
+    catch (error) {
+        console.log(error.message);
+        res.json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+//Get all messages for selected user
+export const getMessages = async (req, res) => {
+    try {
+        const { id: selectedUserId} = req.params;
+        const myId = req.user._id;
+
+        const messages = await Message.find({
+            $or: [
+                {senderId: myId, receiverId: selectedUserId},
+                {senderId: selectedUserId, receiverId: myId},
+            ]
+        })
+
+        await Message.updateMany({
+            senderId: selectedUserId, receiverId: myId
+        }, {seen: true});
+
+        res.json({
+            success: true, messages
+        })
+        
+    } catch (error) {
+        console.log(error.message);
+        res.json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+//api to mark message as seen using message id
+export const markMessageAsSeen = async (req, res) => {
+    try{
+        const {id} = req.params;
+        await Message.findByIdAndUpdate(id, {seen: true})
+        res.json({success: true})
+    }
+    catch (error) {
+        console.log(error.message);
+        res.json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
